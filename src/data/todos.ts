fbe74dbf1e0a9f4aed01db3af024e23ead314146
InesssRibeiro <ineslemosribeiro96@gmail.com>
2025-08-30 20:39:16 +0100

    Todo_List_App
    React, Typescript and Tailwind CSS

---
+++ src/data/todos.ts
@@ -0,0 +1,21 @@
+import type { Todo } from "../types/todos";
+
+const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Like this video!",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Subscribe to Coding in Flow",
+        completed: false
+    },
+    {
+        id: 3,
+        title: "Leave a nice comment!",
+        completed: false
+    }
+]
+
+export default dummyData;
